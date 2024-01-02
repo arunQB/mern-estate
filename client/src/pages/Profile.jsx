e9936b333b41d9d9cefaeabfbc6cb935b7a97a8c
Your Name <arun.joshi@alshaya.com>
2024-01-02 16:28:44 +0530

    complete user lisitng delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -132,7 +132,24 @@ export default function Profile() {
         setShowListingsError(true);
       }
     };
-      
+  
+    const handleListingDelete = async (listingid) => {
+        try {
+          const res = await fetch(`/api/listing/delete/${listingid}`, {
+             method: 'DELETE',
+          });
+        const data = await res.json();
+        if(data.success === false) {
+           console.log(data.message);
+           return;
+        }
+
+        setUserListings((prev) => 
+        prev.filter((listing) => listing._id !== listingid)) 
+        } catch (error) {
+          console.log(error.message);
+        }
+    }; 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -210,7 +227,7 @@ export default function Profile() {
                 </p>
              </Link>
               <div className='flex flex-col item-center'>
-                <button 
+                <button onClick={()=>handleListingDelete(listing._id)}
                 className='text-red-700 uppercase'>Delete</button>
                 <button 
                 className='text-green-700 uppercase'>Edit</button>
